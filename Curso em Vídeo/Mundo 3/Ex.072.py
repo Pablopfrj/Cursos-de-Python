3c3a388b7fa76d1a44f9a82127b1ba304b832d54
Pablo Fernandes <pablopfrj@gmail.com>
2021-04-05 18:42:49 -0300

    ...

--- Curso em Vídeo/Mundo 3/Ex.072.py
+++ Curso em Vídeo/Mundo 3/Ex.072.py
@@ -1,16 +1,17 @@
-num = ('zero' , 'um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','catorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
-pedir_num = int(input('Digite um número entre 0 a 20: '))
-resp = ''
+n = ('zero', 'um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','catorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
 
 while True:
-    while pedir_num not in range(0, 21):
-        pedir_num = int(input('Tente novamente. Digite um número de 0 a 20: '))
-        print(f'Você digitou o número {num[pedir_num]}.')
-        resp = str(input('Quer continuar?[S/N] ')).upper()
-        while resp not in 'SN':
-            resp = str(input('Quer continuar? S/N ')).upper().strip()[0]
-        if resp == 'N':
-            break
+    num = int(input('Digite um número entre 0 e 20: '))
+    if num not in range(0,21):
+        print('Invalido Tente novamente ', end='')
+    else:
+        print(f'Você digitou o número {n[num]}')
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if resp == 'N':
+        break
+
 print('Fim')
 
 
